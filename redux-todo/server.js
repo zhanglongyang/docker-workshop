7dd0945596db2fc6150ac4dbde91ad89a26b2788
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-08-04 21:27:19 +1000

    change port

--- redux-todo/server.js
+++ redux-todo/server.js
@@ -1,13 +1,16 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config')
 
-var app = new (require('express'))()
-var port = 3000
+const app = new(require('express'))()
+const port = 3001
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+const compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
 app.use(webpackHotMiddleware(compiler))
 
 app.get("/", function(req, res) {
@@ -18,6 +21,8 @@ app.listen(port, function(error) {
   if (error) {
     console.error(error)
   } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    console.info(
+      "==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.",
+      port, port)
   }
 })
