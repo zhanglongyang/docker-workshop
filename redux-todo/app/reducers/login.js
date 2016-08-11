4276366867619529abcc44c5a4e4f086ab396ba2
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-08-11 21:23:41 +1000

    connect redux-todo and oauth-server and fix cors issue

--- redux-todo/app/reducers/login.js
+++ redux-todo/app/reducers/login.js
@@ -1,13 +1,19 @@
 const login = (state = false, action) => {
   switch (action.type) {
     case 'LOGIN':
-      console.log('============', state, action)
       fetch('http://localhost:3002/oauth/token', {
-        method: 'POST',
-        mode: 'no-cors'
+        method: 'post',
+        headers: {
+		        'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ZGVtb2NsaWVudDpkZW1vY2xpZW50c2VjcmV0'
+	      },
+        body: 'grant_type=password&username=admin&password=admin'
       }).then(function(res) {
-        console.log(res)
+        return res.json()
+      }).then(function(v) {
+        console.log(v)
       })
+
       return true
     default:
       return state
